9a1124a6782c827ffe0820acce122af8f460ab34
OGNovuh <jamooney2000@gmail.com>
2018-03-02 23:14:50 -0500

    Delete db.js

--- db.js
+++
@@ -1,4 +0,0 @@
-const config = require('./config');
-
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb://${config.mongo_name}:${config.mongo_password}@ds151558.mlab.com:51558/restify-api`);
