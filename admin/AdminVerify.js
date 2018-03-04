64f01534639a85a28521691b559f3ae78f358717
OGNova <jamooney2000@gmail.com>
2018-03-03 20:27:25 -0500

    So I broke token verification. Oops.

--- admin/AdminVerify.js
+++ admin/AdminVerify.js
@@ -4,8 +4,8 @@ const config = require('../config');
 
 function verifyAdminToken(req, res, next) {
   const token = req.headers['token'];
-  if (!token)
-    return res.status(403).send({ auth: false, message: 'No token provided.'});
+  if (!token) return res.status(403).send({ auth: false, message: 'No token provided.'});
+  console.log(config.adminTokens);
   if (!config.adminTokens.includes(token)) return res.status(403).send({ auth: false, message: 'The maze is not for you.' });
   jwt.verify(token, config.secret, function(err, decoded) {
     if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
