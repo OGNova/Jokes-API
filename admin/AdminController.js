94adff955a1d427b275024508577524876a5c8e2
OGNova <jamooney2000@gmail.com>
2018-08-04 21:51:37 -0400

    Rename route.

--- admin/AdminController.js
+++ admin/AdminController.js
@@ -21,9 +21,20 @@ router.get('/:token', function(req, res) {
   });
 });
 
-router.get('/', function(req, res) {
-  if (!req.headers.masterToken) throw new Error('You must supply the master token to get info on other users.');
-  if (req.headers.masterToken !== masterToken) return res.status(403).send('The maze is not for you.');
+router.delete('/:token', function(req, res) {
+  if (!req.headers.mastertoken) throw new Error('You must supply the master token to get info on other users.');
+  if (req.headers.mastertoken != masterToken) return res.status(403).send('The maze is not for you.');
+  if (!req.params.token) throw new Error('Please supply a token to search for.');
+  User.findOneAndRemove({ token: req.params.token }, function(err, user) {
+    if (err) return res.status(500).send(`Something went wrong.\n${err}`);
+    if (!user) return res.status(404).send('No user found.');
+    res.status(200).send(`User ${user.name} was deleted`);
+  });
+});
+
+router.get('/users', function(req, res) {
+  if (!req.headers.mastertoken) throw new Error('You must supply the master token to get info on other users.');
+  if (req.headers.mastertoken != masterToken) return res.status(403).send('The maze is not for you.');
   User.find({}, function(err, users) {
     if (err) return res.status(500).send('There was a problem finding the users.');
     res.status(200).send(users);
