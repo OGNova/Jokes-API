2d492de3de9071f43d9a6e7bc6e9fd1f5850412e
OGNova <jamooney2000@gmail.com>
2018-08-04 21:22:46 -0400

    Actually properly lock admin routes.

--- admin/AdminController.js
+++ admin/AdminController.js
@@ -11,10 +11,10 @@ const User = require('../user/User');
 const { masterToken } = require('../config.js');
 
 router.get('/:token', function(req, res) {
-  if (!req.params.masterToken) throw new Error('You must supply the master token to get info on other users.');
-  if (req.params.masterToken !== masterToken) return res.status(403).send('The maze is not for you.');
+  if (!req.headers.mastertoken) throw new Error('You must supply the master token to get info on other users.');
+  if (req.headers.mastertoken != masterToken) return res.status(403).send('The maze is not for you.');
   if (!req.params.token) throw new Error('Please supply a token to search for.');
-  User.findOne({ token: req.params.userToken }, function(err, user) {
+  User.findOne({ token: req.params.token }, function(err, user) {
     if (err) return res.status(500).send(`There was a problem finding the user.\n${err}`);
     if (!user) return res.status(404).send('No user found.');
     res.status(200).send(user);
@@ -22,9 +22,8 @@ router.get('/:token', function(req, res) {
 });
 
 router.get('/', function(req, res) {
-  if (!req.params.masterToken) throw new Error('You must supply the master token to get info on other users.');
-  if (req.params.masterToken !== masterToken) return res.status(403).send('The maze is not for you.');
-  if (!req.params.token) throw new Error('Please supply a token to search for.');
+  if (!req.headers.masterToken) throw new Error('You must supply the master token to get info on other users.');
+  if (req.headers.masterToken !== masterToken) return res.status(403).send('The maze is not for you.');
   User.find({}, function(err, users) {
     if (err) return res.status(500).send('There was a problem finding the users.');
     res.status(200).send(users);
