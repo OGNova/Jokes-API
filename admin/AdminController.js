2f667d51e31657c30e70a5c7cce51c889d5e6097
OGNova <jamooney2000@gmail.com>
2018-08-04 20:29:03 -0400

    Lock admin routes.

--- admin/AdminController.js
+++ admin/AdminController.js
@@ -8,8 +8,13 @@ router.use(bodyParser.json());
 
 const User = require('../user/User');
 
+const { masterToken } = require('config.js');
+
 router.get('/:token', function(req, res) {
-  User.findOne({ token: req.params.token }, function(err, user) {
+  if (!req.params.masterToken) throw new Error('You must supply the master token to get info on other users.');
+  if (req.params.masterToken !== masterToken) return res.status(403).send('The maze is not for you.');
+  if (!req.params.token) throw new Error('Please supply a token to search for.');
+  User.findOne({ token: req.params.userToken }, function(err, user) {
     if (err) return res.status(500).send(`There was a problem finding the user.\n${err}`);
     if (!user) return res.status(404).send('No user found.');
     res.status(200).send(user);
@@ -17,6 +22,9 @@ router.get('/:token', function(req, res) {
 });
 
 router.get('/', function(req, res) {
+  if (!req.params.masterToken) throw new Error('You must supply the master token to get info on other users.');
+  if (req.params.masterToken !== masterToken) return res.status(403).send('The maze is not for you.');
+  if (!req.params.token) throw new Error('Please supply a token to search for.');
   User.find({}, function(err, users) {
     if (err) return res.status(500).send('There was a problem finding the users.');
     res.status(200).send(users);
